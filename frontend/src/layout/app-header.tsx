e546fa4e0260e5de166821df7f53af2ab1ccea1e
Andrey Savka <a_sawka@yahoo.com>
2025-04-08 00:07:23 +0300

    add left panel

--- frontend/src/layout/app-header.tsx
+++ frontend/src/layout/app-header.tsx
@@ -1,24 +1,26 @@
+
 import { FC } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import './app-header.scss';
 
-const AppHeader: FC = () => {
+interface AppHeaderProps {
+  toggleSidePanel: () => void;
+}
 
-  return <Navbar expand="lg" className="app-header">
-    <Container>
-      <Navbar.Brand href="/#/" className="header-title">
+const AppHeader: FC<AppHeaderProps> = ({ toggleSidePanel }) => {
+  return <Navbar expand="lg" className="app-header fixed-top">
+      <button 
+        className="me-2 burger-button" 
+        onClick={toggleSidePanel}
+      >
+        <FaBars size={20} />
+      </button>
+      <Navbar.Brand as={Link} to="/" className="header-title">
         Expense tracking app
       </Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav>
-          <Nav.Link href="/#/about">About</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
   </Navbar>
-
 }
 
 export default AppHeader;
